93c5ce4dfa4c606593f10ae2c7b8c98501f81ef2
QuatermainTz <quatermaintz2@gmail.com>
2026-09-16 21:58:26 +0300

    Remove duplicate bootstrap.php require - AppFactory loads it internally

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@
 use SalesRender\Plugin\Core\Logistic\Factories\WebAppFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../bootstrap.php';
 
 $factory = new WebAppFactory();
 $application = $factory->build();
