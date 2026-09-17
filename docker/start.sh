55a9f4928f942bb1e8e532c1ca86495143196c6c
QuatermainTz <quatermaintz2@gmail.com>
2026-09-17 10:50:07 +0300

    Fix db file permissions for php-fpm worker user

--- docker/start.sh
+++ docker/start.sh
@@ -5,5 +5,7 @@ sed "s/PORT_PLACEHOLDER/${PORT:-10000}/" /app/docker/nginx.conf > /etc/nginx/htt
 
 php /app/console.php db:create || true
 
+chmod -R 777 /app/db /app/runtime
+
 php-fpm -D
 nginx -g "daemon off;"
