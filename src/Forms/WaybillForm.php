cd983d653f6e28bcf175b23673172063e1be360b
QuatermainTz <quatermaintz2@gmail.com>
2026-09-18 11:30:43 +0300

    Fix class name: IntegerDefinition, not NumberDefinition

--- src/Forms/WaybillForm.php
+++ src/Forms/WaybillForm.php
@@ -3,7 +3,7 @@
 namespace MyVendor\Plugin\FulfillFlow\Forms;
 
 use SalesRender\Plugin\Components\Form\FieldDefinitions\StringDefinition;
-use SalesRender\Plugin\Components\Form\FieldDefinitions\NumberDefinition;
+use SalesRender\Plugin\Components\Form\FieldDefinitions\IntegerDefinition;
 use SalesRender\Plugin\Components\Form\FieldGroup;
 use SalesRender\Plugin\Components\Form\Form;
 
@@ -29,8 +29,8 @@ class WaybillForm extends Form
                     [
                         'productSku' => new StringDefinition('Product SKU', 'FulfillFlow product_sku', $nonEmpty),
                         'warehouseName' => new StringDefinition('Warehouse', 'FulfillFlow warehouse_name', $nonEmpty),
-                        'quantity' => new NumberDefinition('Quantity', 'Number of units', $nonEmpty),
-                        'shippingCharges' => new NumberDefinition('Shipping charges', 'Delivery fee', $noValidation),
+                        'quantity' => new IntegerDefinition('Quantity', 'Number of units', $nonEmpty),
+                        'shippingCharges' => new IntegerDefinition('Shipping charges', 'Delivery fee', $noValidation),
                     ]
                 ),
             ],
